267a9f6e46ad90bd6ebd801313846c2ced22d6ab
Hassan Ghaed <mhaghaed@gmail.com>
2021-01-20 23:19:17 -0800

    Added delete functionality

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.get('/', function(req, res, next){
   res.render('searchusers');
 })
 
-
 // Search Processing
 app.post('/user/search', function(req, res, next){
   let id = req.body.id;
@@ -50,8 +49,40 @@ app.post('/user/search', function(req, res, next){
       })
     }
   })
-
 })
+
+// Add User Page
+app.get('/user/add', function(req, res, next){
+  res.render('adduser');
+})
+
+// Process User Page
+app.post('/user/add', function(req, res, next){
+  let id = req.body.id;
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let email = req.body.email;
+  let phone = req.body.phone;
+
+  client.hmset(id, [
+    'first_name', first_name,
+    'last_name', last_name,
+    'email', email,
+    'phone', phone
+  ], function(err, reply){
+    if(err) {
+      console.log(err);
+    }
+    console.log(reply);
+    res.redirect('/');
+  })
+})
+
+// Delete User
+app.delete('/user/delete/:id', function(req, res, next){
+  client.del(req.params.id);
+  res.redirect('/');
+});
 
 app.listen(port, function(){
   console.log('Server started on port '+port);
